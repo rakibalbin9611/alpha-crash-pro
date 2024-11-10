d8134eb03755e3aa1b5575a30266bf1767a9d413
rakibalbin9611 <rakibalbin17@gmail.com>
2024-11-10 13:31:56 +0600

    ui design completed

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,5 @@
+function play() {
+  // hide the home section when click play button to add hidden class in help of (classList)
+  const homeSection = document.getElementById("home-screen");
+  homeSection.classList.add("hidden");
+}
